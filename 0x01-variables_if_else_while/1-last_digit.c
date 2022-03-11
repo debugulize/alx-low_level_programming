29fb4f49ea5b5b6a03ec04d291e6c320481189bb
debugulize <wanjikumary631@gmail.com>
2022-03-11 01:34:38 -0800

    six is greater that 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,35 +1,24 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- *
+ * main - prints the last digit of the random
+ * number stored in the variable n
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n, s;
+	int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /* your code goes there */
-        s = n % 10;
-        if (s > 5)
-        {
-        printf("Last digit of %d is %d and is greater than 5\n", n, s);
-        }
-        if (n == 0);
-        else if (s == 0)
-
-        {
-        printf("Last digit of %d is %d and is 0\n", n, s);
-        }
-
-        if (n < 6);
-        else if (s < 6)
-
-        {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, s);
-        }
-        return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
 }
