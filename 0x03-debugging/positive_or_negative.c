6ec99d012481eb6be7afec2988774613f2a03b25
debugulize <wanjikumary631@gmail.com>
2022-03-15 10:18:57 -0700

    mai n file positive or negative

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
+void positive_or_negative (int i)
+{
+
+	srand(time(0));
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+
+}
